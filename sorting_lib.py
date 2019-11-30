6410d84ab7f1c6c9beeb0b451b8585375482ad1d
Gabriele2396 <56448402+Gabriele2396@users.noreply.github.com>
2019-11-30 11:11:15 +0100

    sorting_lib.py complete

--- sorting_lib.py
+++ sorting_lib.py
@@ -87,5 +87,56 @@ def CountingSort_let(letters):
 
 ## Implementation of Counting Sort to get the ordered list of words.
 
-
+def CountingSort_wor(words):
+    output = list()
+    m = len(words)
     
+    for i in range(m): 
+        if i == 0: # start comparing words appending first word
+            output.append(words[i])
+        else:
+            # checking and sorting words 
+            for k in range(len(output)):
+                j = 0
+                # looking for the first 2 different letters
+                while j < len(words[i])-1 and j < len(output[k])-1 and words[i][j] == output[k][j]:
+                    j += 1
+                
+                # all strings are ordered according to their first word
+                if output[k][j] == ' ' and words[i][j] != ' ':
+                    new_out = [output[k], words[i]]
+                    if k == len(output)-1:
+                        output.append(words[i])
+                        break           
+      
+                elif output[k][j] != ' ' and words[i][j] == ' ':
+                    new_out = [words[i], output[k]]
+                    output.insert(k, words[i])
+                    break
+
+                elif j != len(output[k])-1 and j == len(words[i])-1 and words[i][j] == output[k][j]:
+                    new_out = [words[i], output[k]]
+                    output.insert(k, words[i])
+                    break                
+                
+                # priority to short words
+                elif j == len(output[k])-1 and j != len(words[i])-1 and words[i][j] == output[k][j]:
+                    new_out = [output[k], words[i]] 
+                    if k == len(output)-1:
+                        output.append(words[i])
+                        break           
+
+                # sorting the single letters
+                else:
+                    x = [words[i][j], output[k][j]]
+                    new_out = CountingSort_let(x) # recalling function for sorting letters
+                    
+                    if new_out == x:
+                        output.insert(k, words[i])
+                        break
+                    else:
+                        if k == len(output)-1:
+                            output.append(words[i])
+                            break                
+
+    return output
